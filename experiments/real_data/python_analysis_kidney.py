a05b7512a0cc4d3461a780e1b7e211de1eb0c327
David Rindt <rindt@falconet.stats.ox.ac.uk>
2021-02-10 11:50:54 +0000

    updated kidney experiment

--- experiments/real_data/python_analysis_kidney.py
+++ experiments/real_data/python_analysis_kidney.py
@@ -19,15 +19,15 @@ kidney = pd.read_csv('../../data/kidney.csv')
 kidney_male = kidney[kidney.sex == 1]
 kidney_female = kidney[kidney.sex == 2]
 
-kidney.head(10)
+print(kidney.head(10))
 
+
+# Test if age has relationship with time in each of the subpopulations
 x_female, z_female, d_female = kidney_female.age.values[:,None], kidney_female.time.values, kidney_female.status.values
 survival_scatter_plot(x=x_female.flatten(), d=d_female, z=z_female)
-plt.show()
 
 x_male, z_male, d_male = kidney_male.age.values[:,None], kidney_male.time.values, kidney_male.status.values
 survival_scatter_plot(x=x_male.flatten(), d=d_male, z=z_male)
-plt.show()
 
 v, p = wild_bootstrap_LR.wild_bootstrap_test_logrank_covariates(x=x_female, z=z_female, d=d_female, kernel_x='lin', kernel_z='con')
 print('lin, con', p, v)
@@ -35,3 +35,26 @@ v, p = wild_bootstrap_LR.wild_bootstrap_test_logrank_covariates(x=x_female, z=z_
 print('gau, con', p, v)
 v, p = wild_bootstrap_LR.wild_bootstrap_test_logrank_covariates(x=x_female, z=z_female, d=d_female, kernel_x='gau', kernel_z='gau')
 print('gua, gau', p, v)
+
+
+
+# Overall test if time depends on (age, sex, frail).
+
+X = kidney[['age', 'sex', 'frail']]
+print(X)
+X = np.array(X)
+print(X)
+z = kidney.time.values
+d = kidney.status.values
+
+B = int(10e6)
+v, p = wild_bootstrap_LR.wild_bootstrap_test_logrank_covariates(x=X, z=z, d=d, kernel_x='lin', kernel_z='con', num_bootstrap_statistics=B)
+print('lin, con', p, v)
+v, p = wild_bootstrap_LR.wild_bootstrap_test_logrank_covariates(x=X, z=z, d=d, kernel_x='gau', kernel_z='con', num_bootstrap_statistics=B)
+print('gau, con', p, v)
+v, p = wild_bootstrap_LR.wild_bootstrap_test_logrank_covariates(x=X, z=z, d=d, kernel_x='gau', kernel_z='gau', num_bootstrap_statistics=B)
+print('gua, gau', p, v)
+v, p = wild_bootstrap_LR.wild_bootstrap_test_logrank_covariates(x=X, z=z, d=d, kernel_x='linfis', kernel_z='con', num_bootstrap_statistics=B)
+print('linfis, con', p, v)
+v, p = wild_bootstrap_LR.wild_bootstrap_test_logrank_covariates(x=X, z=z, d=d, kernel_x='linfis', kernel_z='gau', num_bootstrap_statistics=B)
+print('linfis, gau', p, v)
